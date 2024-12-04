2c38ac109afa8ea8299a616dcf81e6881c686b63
Soufiane <soufiane.chaoufi@gmail.com>
2024-12-03 20:48:49 -0800

    Refactor meal and about pages; add error and not found components; update styles and dependencies

--- components/meals/meal-item.jsx
+++ components/meals/meal-item.jsx
@@ -1,14 +1,19 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import classes from "./styles.module.css";
+import classes from "./meal-item.module.css";
 
 export default function MealItem({ title, slug, image, summary, creator }) {
+  console.log({ title, slug, image, summary, creator });
   return (
     <article className={classes.meal}>
       <header>
         <div className={classes.image}>
-          <Image src={image} alt={title} fill />
+          <Image
+            src={`https://soufianechaoufi-nextjs.s3.amazonaws.com/${image}`}
+            alt={title}
+            fill
+          />
         </div>
         <div className={classes.headerText}>
           <h2>{title}</h2>
